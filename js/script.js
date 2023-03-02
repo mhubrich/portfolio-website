3d1b28c4785931e1ecb2fc48837fb64d5fbccd23
mhubrich <mhubrich.2011@gmail.com>
2023-03-01 16:24:29 -0800

    add typing animation in home section

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* Typing animation in home section */
+var typed = new Typed(".typing", {
+    strings: ["Product Manager.", "Software Developer.", "Data Analyst.", "Graphic Designer.", "Mentor.", "Athlete.", "Friend."],
+    typeSpeed: 30,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true
+})
