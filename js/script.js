cc639ebd2ea235263614e168cdf8e28f2a407cd4
mhubrich <mhubrich.2011@gmail.com>
2023-03-05 14:38:53 -0800

    add portfolio section (html)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* Typing animation in home section */
 var typed = new Typed(".typing", {
-    strings: ["Product Manager.", "Software Developer.", "Data Analyst.", "Graphic Designer.", "Mentor.", "Athlete.", "Friend."],
+    strings: ["Product Manager.", "Software Developer.", "Data Analyst.", "Mentor.", "Athlete.", "Friend."],
     typeSpeed: 30,
     backSpeed: 10,
     backDelay: 1000,
